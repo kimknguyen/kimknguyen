3dff16faa90313938973e766a4c34549cb4cfc8c
Kim Nguyen <kkn2011@nyu.edu>
2022-03-31 11:06:37 -0400

    little change

--- Button.js
+++ Button.js
@@ -7,15 +7,23 @@ const myButton = document.getElementById("theButton");
 
 myButton.addEventListener('click', magicClick)
 
+//const link = document.createElement('a');
+//link.id = 'scary'; 
+//link.href = 'https://media.giphy.com/media/5oWpOD8Thsmo8/giphy.gif'; 
+//document.getElementById('scary').click();
+
 function magicClick(){
     console.log("you clicked!")
     theBody.style.backgroundColor = "black";
     theBody.style.color = "white"; 
     myButton.style.backgroundColor = "black"; 
     document.querySelector("h3").textContent = "WHY DID YOU CLICK?!"
+    //theButton.style.visibility = 'none'; 
+    //string.link("https://media.giphy.com/media/5oWpOD8Thsmo8/giphy.gif")
 }
 
-//function setIntput(){
-    //myButton.style.visibility = "hidden"; 
-    //}
 
+
+
+
+//https://www.codegrepper.com/code-examples/javascript/how+to+add+link+in+javascript
